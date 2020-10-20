54e38117b888c039c733ef1eb56e3b06e3a2ed35
sinchanaTR <72921199+sinchanaTR@users.noreply.github.com>
2020-10-20 21:27:03 +0530

    My third commit

---
+++ Berlin.java
@@ -0,0 +1,11 @@
+public class Berlin
+{
+public static void main(String[] param)
+{
+	greet("welcome");
+}
+public static void greet(String greeet)
+{
+	System.out.println("hello:"+greeet);
+}
+}
---
+++ Nairobi.java
@@ -0,0 +1,18 @@
+public class Nairobi
+{
+	public static void main(String[] param)
+	{
+		printEmail("sinchana.sj59@gmail.com");
+		printPl("Java");
+		
+	}
+	public static void printEmail(String name)
+	{
+		System.out.println("Email:" +name);
+	}
+	public static void printPl(String language)
+	{
+		System.out.println("PL:" +language);
+	}
+	
+}
