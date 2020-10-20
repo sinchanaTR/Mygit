c08942081b04fc8a00c546f37d771c996ce64a83
sinchanaTR <72921199+sinchanaTR@users.noreply.github.com>
2020-10-20 21:24:21 +0530

    Add files via upload

---
+++ Helsinky.java
@@ -0,0 +1,13 @@
+public class Helsinky
+{
+public static void main(String[] param)
+{
+ String phoneno="7892679276";
+  final String adharno="8245151510";
+ String email="sinchana.sj59@gmail.com";
+ System.out.println("Phone number:" +phoneno);
+ System.out.println("Adhar no:" +adharno);
+ System.out.println("Email:" +phoneno);
+ 
+}
+}
